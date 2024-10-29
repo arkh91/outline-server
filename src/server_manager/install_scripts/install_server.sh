5950fee5fb310e40d8c606314f0fd76b5889ac11
Ali <39136692+arkh91@users.noreply.github.com>
2024-10-28 19:19:50 -0700

    Update install_server.sh

--- src/server_manager/install_scripts/install_server.sh
+++ src/server_manager/install_scripts/install_server.sh
@@ -537,8 +537,9 @@ brackets) into Step 2 of the Outline Manager interface:
 
 $(echo -e "\033[1;32m{\"apiUrl\":\"$(get_field_value apiUrl)\",\"certSha256\":\"$(get_field_value certSha256)\"}\033[0m")
 
-echo "{\"apiUrl\":\"$(get_field_value apiUrl)\",\"certSha256\":\"$(get_field_value certSha256)\"}" >> /opt/outline/installation_output
-echo -e "\033[0;35mThe installation_output is stored in /opt/outline/installation_output\033[0m"
+cat "{\"apiUrl\":\"$(get_field_value apiUrl)\",\"certSha256\":\"$(get_field_value certSha256)\"}" >> /opt/outline/installation_output
+$(echo -e "\033[0;35mThe installation_output is stored in /opt/outline/installation_output\033[0m")
+#echo -e "${PURPLE}User '$USERNAME' already exists.${NO_COLOR}"
 
 ${FIREWALL_STATUS}
 END_OF_SERVER_OUTPUT
